7c0b38cf4ad429fbeee10671f4b29686dda4603a
shrief-mohamed-ramadan <shrief_elsamaloty35@yahoo.com>
2019-04-25 18:25:40 +0200

    Add project files.

---
+++ pms/Controllers/ProjectController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace pms.Controllers
+{
+    public class ProjectController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult GetProject()
+        {
+            return Json(new { status = "200", data = "Blech", displaySweetAlert = false });
+        }
+    }
+}
---
+++ pms/Controllers/UserController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace pms.Controllers
+{
+    public class UserController : Controller
+    {
+        public ActionResult CustomerHome()
+        {
+            return View("~/Views/Customer/Index.cshtml");
+        }
+
+        public ActionResult Profile()
+        {
+            return View("~/Views/User/Profile.cshtml");
+        }
+    }
+}
